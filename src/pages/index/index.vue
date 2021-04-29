b3249d820140d47ffdae01552e54f8da54d90bb1
qinzhen3 <qinzhen3@example.com>
2021-04-29 22:19:55 +0800

    init project

---
+++ src/pages/index/index.vue
@@ -0,0 +1,33 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+  import card from '@/components/card';
+
+  export default {
+    data() {
+      return {
+        motto: 'Hello miniprograme',
+        userInfo: {
+          nickName: 'mpvue',
+          avatarUrl: 'http://mpvue.com/assets/logo.png'
+        }
+      };
+    },
+
+    components: {
+      card
+    },
+
+    methods: {
+      upload() {}
+    },
+
+    created() {
+      // let app = getApp()
+    }
+  };
+</script>
+
+<style scoped></style>
